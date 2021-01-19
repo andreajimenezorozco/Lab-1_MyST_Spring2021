60e52158ba722ccef78adf0d516ee9c783dd2ab6
Andrea Jimenez <andrea.jimenez@ieseg.fr>
2021-01-19 10:10:11 -0600

    Update

--- main.py
+++ main.py
@@ -15,7 +15,13 @@ from data import *
 
 # IPC TICKERS FROM YAHOO FINANCE
 
-tickers = []
+tickers = ["KIMBERA.MX", "BBAJIOO.MX", "BIMBOA.MX", "ALSEA.MX", "AMXL.MX", "AC.MX",
+         "MEGACPO.MX", "GCARSOA1.MX", "FEMSAUBD.MX", "GAPB.MX", "GFNORTEO.MX",
+         "GRUMAB.MX", "TLEVISACPO.MX", "CUERVO.MX", "GENTERA.MX", "ASURB.MX", "PINFRA.MX",
+          "OMAB.MX", "IENOVA.MX", "LABB.MX", "GMEXICOB.MX", "BOLSAA.MX", "CEMEXCPO.MX", 'LIVEPOLC-1.MX',
+         "KOFUBL.MX", "PE&OLES.MX", "SITESB-1.MX", "Q.MX", "ELEKTRA.MX",
+         "ORBIA.MX", 'NEMAKA.MX', "ALFAA.MX", "WALMEX.MX", "VESTA.MX", "GFINBURO.MX"]
+
 start, end = '2018-01-01', '2020-12-01'
 daily_closes = get_closes(tickers, start, end)
 
