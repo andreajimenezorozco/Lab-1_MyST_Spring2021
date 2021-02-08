a3eb1418ee555e624f7ac9b48c193b5396b8b4a3
Andrea Jimenez <andrea.jimenez@ieseg.fr>
2021-02-07 22:37:49 -0600

    Update

--- data.py
+++ data.py
@@ -12,6 +12,8 @@
 import glob
 import pandas as pd
 import os
+import pandas_datareader.data as web
+import numpy as np
 
 
 def multiple_csv(path):
@@ -27,22 +29,58 @@ def multiple_csv(path):
     return all_df
 
 
-def cleaning_data(df):
-    df['Ticker'] = [i.replace('*', '') for i in df['Ticker']]
-    df['Ticker'] = df['Ticker'] + '.MX'
-    df['Peso (%)'] = df['Peso (%)'] / 100
+def get_closes(tickers, start_date=None, end_date=None, freq=None):
+    closes = pd.DataFrame(columns=tickers, index=web.YahooDailyReader(tickers[0], start=start_date, end=end_date
+                                                                      , interval=freq).read().index)
+    for ticker in tickers:
+        df = web.YahooDailyReader(symbols=ticker, start=start_date, end=end_date, interval=freq).read()
+        closes[ticker] = df['Adj Close']
+    closes.index_name = 'Date'
+    closes = closes.sort_index()
+    return closes
 
-    # Tickers
-    df['Ticker'] = df['Ticker'].replace('LIVEPOLC.1.MX', 'LIVEPOLC-1.MX')
-    df['Ticker'] = df['Ticker'].replace('MEXCHEM.MX', 'ORBIA.MX')
-    df['Ticker'] = df['Ticker'].replace('SITESB.1.MX', 'SITESB-1.MX')
-    df['Ticker'] = df['Ticker'].replace('GFREGIOO.MX', 'RA.MX')
-    df['Ticker'] = df['Ticker'].replace('NMKA.MX', 'NEMAKA.MX')
 
-    # Remover tickers para CASH
-    tickers_drop = ['KOFL.MX', 'BSMXB.MX', 'MXN.MX', 'USD.MX', '\xa0.MX', 'KOFUBL.MX']
-    rows = list(df[list(df['Ticker'].isin(tickers_drop))].index)
-    df.drop(rows, inplace=True)
-    return df.set_index('Fecha')
+def get_open(tickers, start_date=None, end_date=None, freq=None):
+    open_ = pd.DataFrame(columns=tickers, index=web.YahooDailyReader(tickers[0], start=start_date, end=end_date
+                                                                      , interval=freq).read().index)
+    for ticker in tickers:
+        df = web.YahooDailyReader(symbols=ticker, start=start_date, end=end_date, interval=freq).read()
+        open_[ticker] = df['Open']
+    open_.index_name = 'Date'
+    open_ = open_.sort_index()
+    return open_
+
+
+def global_dataframe(historical, closes, open_, dates):
+    fix_dates = sorted(list(set(closes.index.astype(str).tolist()) & set(dates)))
+    fix_prices_closes = closes.iloc[[int(np.where(closes.index.astype(str) == i)[0]) for i in fix_dates]]
+    fix_prices_closes = fix_prices_closes.reindex(sorted(fix_prices_closes.columns), axis=1)
+
+    fix_prices_open = open_.iloc[[int(np.where(open_.index.astype(str) == i)[0]) for i in fix_dates]]
+    fix_prices_open = fix_prices_open.reindex(sorted(fix_prices_open.columns), axis=1)
+
+    historical_dates = {}
+    for date in fix_dates:
+        historical_dates[date] = historical[historical.index == date].reset_index().set_index('Ticker').T
+
+    for date in fix_dates:
+        historical_dates[date].loc['Close'] = 0
+        for ticker in fix_prices_closes.columns:
+            historical_dates[date].loc['Close'][ticker] = fix_prices_closes.loc[date][ticker]
+            historical_dates[date].columns.sort_values()
+
+    for date in fix_dates:
+        historical_dates[date].loc['Open'] = 0
+        for ticker in fix_prices_open.columns:
+            historical_dates[date].loc['Open'][ticker] = fix_prices_open.loc[date][ticker]
+            historical_dates[date].columns.sort_values()
+
+    df_list = [v for k, v in historical_dates.items()]
+    df = pd.concat(df_list, axis=1)
+    df = df.T
+    df = df.reset_index()
+
+    return df
+
 
 
--- visualizations.py
+++ visualizations.py
@@ -8,3 +8,20 @@
 # -- repository: YOUR REPOSITORY URL                                                                     -- #
 # -- --------------------------------------------------------------------------------------------------- -- #
 """
+import matplotlib.pyplot as plt
+
+
+def basic_plot(df, title, column, title_x, title_y):
+    plt.figure(figsize=(15,4))
+    ax = plt.axes()
+    ax.spines["right"].set_visible(False)
+    ax.spines["left"].set_visible(False)
+    ax.spines["bottom"].set_visible(False)
+    ax.spines["top"].set_visible(False)
+    plt.plot(df[column], lw=3,color="green", label=column)
+    plt.legend(loc='best', fontsize=8)
+    plt.title(title, fontsize=15)
+    plt.xlabel(title_x, fontsize=8)
+    plt.ylabel(title_y, fontsize=8)
+    plt.show()
+    return plt.show()
